2da21bfadb8d21cbc34eedf00f307897238da71f
Bernarda Brkić <bbrkic@tvz.hr>
2021-11-19 17:55:30 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,9 +16,9 @@ new Vue({
 
 Vue.config.productionTip = false
 
-let bar1 =  document.querySelector('.bar1');
-let bar2 =  document.querySelector('.bar2');
-let bar3 =  document.querySelector('.bar3');
+let bar1 = document.querySelector('.bar1');
+let bar2 = document.querySelector('.bar2');
+let bar3 = document.querySelector('.bar3');
 let menubar = document.querySelector('.menubar');
 let links = document.querySelector('.links');
 
