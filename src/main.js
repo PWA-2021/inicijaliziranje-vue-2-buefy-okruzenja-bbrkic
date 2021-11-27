54af7ebfc0416aecc1a392a719690902ead50918
Bernarda Brkić <bbrkic@tvz.hr>
2021-11-27 21:09:54 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //buefy
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
 
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
@@ -11,22 +12,21 @@ Vue.use(Buefy);
 Vue.use(VueTypedJs);
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-let bar1 = document.querySelector('.bar1');
-let bar2 = document.querySelector('.bar2');
-let bar3 = document.querySelector('.bar3');
-let menubar = document.querySelector('.menubar');
-let links = document.querySelector('.links');
+let bar1 = document.querySelector(".bar1");
+let bar2 = document.querySelector(".bar2");
+let bar3 = document.querySelector(".bar3");
+let menubar = document.querySelector(".menubar");
+let links = document.querySelector(".links");
 
-menubar.addEventListener("click" , function() {
-  bar1.classList.toggle('barclose1');
-  bar2.classList.toggle('barclose2');
-  bar3.classList.toggle('barclose3');
-  links.classList.toggle('newlinks');
+menubar.addEventListener("click", function () {
+  bar1.classList.toggle("barclose1");
+  bar2.classList.toggle("barclose2");
+  bar3.classList.toggle("barclose3");
+  links.classList.toggle("newlinks");
 });
-
-
