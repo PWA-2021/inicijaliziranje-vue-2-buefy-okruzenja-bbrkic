77eb91d53d7200db6ec98913a2a2f2239e44b8f5
Bernarda Brkić <bbrkic@tvz.hr>
2021-12-02 21:49:09 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 // import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import Data from "../views/Data.vue";
 
 Vue.use(VueRouter);
 
@@ -12,15 +13,15 @@ const routes = [
     name: "Home",
     component: Home,
   },
-  // {
-  //   path: "/login",
-  //   name: "Login",
-  //   component: Login,
-  // },
   {
     path: "/register",
     name: "Register",
     component: Register,
+  },
+  {
+    path: "/data",
+    name: "Data",
+    component: Data,
   },
 ];
 
