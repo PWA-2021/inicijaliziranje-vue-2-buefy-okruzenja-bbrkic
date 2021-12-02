f68faa9bf721009068b9a70bcd1a3ddd3171cab5
Bernarda Brkić <bbrkic@tvz.hr>
2021-12-02 21:49:12 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -41,4 +41,15 @@ document
   .addEventListener("click", function () {
     document.querySelector(".popup").classList.remove("active");
     document.querySelector(".dark-bg").classList.remove("active");
-  });
+});
+
+// SCROLL TO TOP
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else{
+    toTop.classList.remove("active");
+  }
+})
