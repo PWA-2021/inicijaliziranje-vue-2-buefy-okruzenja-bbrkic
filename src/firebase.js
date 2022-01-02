594222f3539cccfeba045019721608c9d63cb049
Bernarda Brkić <bbrkic@tvz.hr>
2022-01-02 21:36:02 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDEp2GnsZW26AMrQ5uvBDbqdccGlWZIzTU",
+//   authDomain: "captaincoin-169ac.firebaseapp.com",
+//   databaseURL: "https://captaincoin-169ac-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "captaincoin-169ac",
+//   storageBucket: "captaincoin-169ac.appspot.com",
+//   messagingSenderId: "351537290089",
+//   appId: "1:351537290089:web:1d04c93df5c48243e57bf8",
+//   measurementId: "G-ZGP6PZQ5K4"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
