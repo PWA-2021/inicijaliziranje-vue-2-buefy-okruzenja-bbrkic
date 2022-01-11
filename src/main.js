6112d9b41cd9f09c32bcb2c171acb53363fb3e50
Bernarda Brkić <bbrkic@tvz.hr>
2022-01-12 00:54:25 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -81,24 +81,6 @@ document
 
 // SCROLL TO TOP
 const toTop = document.querySelector('.to-top')
-
-function handleFormSubmit(event) {
-  event.preventDefault()
-
-  const data = new FormData(event.target)
-
-  const formJSON = Object.fromEntries(data.entries())
-
-  // for multi-selects, we need special handling
-  formJSON.snacks = data.getAll('snacks')
-
-  const results = document.querySelector('.results pre')
-  results.innerText = JSON.stringify(formJSON, null, 2)
-}
-
-const form = document.querySelector('.contact-form')
-form.addEventListener('submit', handleFormSubmit)
-
 window.addEventListener('scroll', () => {
   if (window.pageYOffset > 100) {
     toTop.classList.add('active')
